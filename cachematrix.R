9faf0b044ed435bc019c5a0f3efbc17d005bf0fa
lafona <lafona@gmail.com>
2015-11-20 20:51:43 -0500

    Updated cachematrix.R file for Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##Creates a list which contains the functions necessary for manipulation of the matrix x.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    set<-function(y){
+        x<<-y
+        m<<- NULL
+    }
+    get<- function () x
+    setinv<- function(solve) m<<- solve
+    getinv<- function() m
+    list(set =set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
+## Calculates the inverse of matrix 'x', using cached inverse data if the function has been run previously
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$getinv()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data<- x$get()
+    m<- solve(data, ...)
+    x$setinv(m)
+    m
 }
